54c757b3af864925066dd4fe708e4b0f79fb13ee
Dustin Brett <dustinbrett@gmail.com>
2021-05-31 22:07:21 -0700

    Constant for click timeout

--- components/system/useDoubleClick.ts
+++ components/system/useDoubleClick.ts
@@ -1,11 +1,15 @@
 import { useCallback, useRef } from 'react';
+import { DOUBLE_CLICK_TIMEOUT_IN_MILLISECONDS } from 'utils/constants';
 
 type DoubleClick = (
   handler: React.MouseEventHandler,
   timeout?: number
 ) => React.MouseEventHandler;
 
-const useDoubleClick: DoubleClick = (handler, timeout = 500) => {
+const useDoubleClick: DoubleClick = (
+  handler,
+  timeout = DOUBLE_CLICK_TIMEOUT_IN_MILLISECONDS
+) => {
   const timer = useRef<NodeJS.Timeout | null>(null);
   const onClick = useCallback<React.MouseEventHandler>(
     (event) => {
