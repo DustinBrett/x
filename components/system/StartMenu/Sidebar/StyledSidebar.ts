4195bd2f7a80daac5ebc77e9aeb6f16ba66b15e6
Dustin Brett <dustinbrett@gmail.com>
2021-06-07 23:38:55 -0700

    Icons for start menu and svg tweaks

--- components/system/StartMenu/Sidebar/StyledSidebar.ts
+++ components/system/StartMenu/Sidebar/StyledSidebar.ts
@@ -4,12 +4,29 @@ const StyledSidebar = styled.nav`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  padding-top: 4px;
 
   li {
     border: 1px solid transparent;
+    display: flex;
     height: 48px;
     margin-right: 7px;
+    place-content: center;
+    place-items: center;
     width: 48px;
+
+    figure {
+      svg {
+        fill: #fff;
+        height: 16px;
+        margin-left: 1px;
+        width: 16px;
+      }
+
+      figcaption {
+        display: none;
+      }
+    }
 
     &:hover {
       background-color: hsla(0, 0%, 35%, 70%);
--- components/system/StartMenu/Sidebar/index.tsx
+++ components/system/StartMenu/Sidebar/index.tsx
@@ -1,16 +1,40 @@
+import {
+  AllApps,
+  Documents,
+  Power,
+  SideMenu
+} from 'components/system/StartMenu/Sidebar/SidebarIcons';
 import StyledSidebar from 'components/system/StartMenu/Sidebar/StyledSidebar';
+
+type SidebarButtonProps = {
+  name: string;
+  icon: JSX.Element;
+};
+
+const topButtons = [
+  { name: 'START', icon: <SideMenu /> },
+  { name: 'All apps', icon: <AllApps /> }
+];
+
+const bottomButtons = [
+  { name: 'Documents', icon: <Documents /> },
+  { name: 'Power', icon: <Power /> }
+];
+
+const SidebarButton = ({ name, icon }: SidebarButtonProps) => (
+  <li key={name}>
+    <figure>
+      {icon}
+      <figcaption>{name}</figcaption>
+    </figure>
+  </li>
+);
 
 const Sidebar = (): JSX.Element => (
   <StyledSidebar>
-    <ol>
-      <li>START</li>
-      <li>All apps</li>
-    </ol>
-    <ol>
-      <li>Dustin</li>
-      <li>Documents</li>
-      <li>Power</li>
-    </ol>
+    {Object.entries({ topButtons, bottomButtons }).map(([key, buttons]) => (
+      <ol key={key}>{buttons.map(SidebarButton)}</ol>
+    ))}
   </StyledSidebar>
 );
 
