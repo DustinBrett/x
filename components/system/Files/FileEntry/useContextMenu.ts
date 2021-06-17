c693595b485be30f6eb6d74ebee76110a2828e88
Dustin Brett <dustinbrett@gmail.com>
2021-06-16 21:31:37 -0700

    Open With logic

--- components/system/Files/FileEntry/useContextMenu.ts
+++ components/system/Files/FileEntry/useContextMenu.ts
@@ -1,6 +1,8 @@
+import { extensions } from 'components/system/Files/FileEntry/functions';
 import useFile from 'components/system/Files/FileEntry/useFile';
 import type { MenuItem } from 'contexts/menu/useMenuContextState';
 import processDirectory from 'contexts/process/directory';
+import { extname } from 'path';
 
 const useContextMenu = (
   url: string,
@@ -8,21 +10,30 @@ const useContextMenu = (
   deleteFile: () => void,
   renameFile: () => void
 ): MenuItem[] => {
+  const { process: [, ...openWith] = [] } = extensions[extname(url)] || {};
   const { icon: pidIcon } = processDirectory[pid] || {};
-  const openFile = useFile(url, pid);
+  const openFile = useFile(url);
   const menuItems: MenuItem[] = [
     { label: 'Delete', action: deleteFile },
     { label: 'Rename', action: renameFile }
   ];
 
   if (pid) {
+    const openWithEntries = openWith.map((id) => {
+      const { icon, title: label } = processDirectory[id] || {};
+      const action = () => openFile(id);
+
+      return { icon, label, action };
+    });
+
     menuItems.unshift(
       {
         icon: pidIcon,
         label: 'Open',
         primary: true,
-        action: openFile
+        action: () => openFile(pid)
       },
+      ...openWithEntries,
       { separator: 1 }
     );
   }
