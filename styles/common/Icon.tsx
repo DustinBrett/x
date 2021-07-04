9e81cd786be65fb2c394806ceb0b3bc696a5fac2
Dustin Brett <dustinbrett@gmail.com>
2021-07-04 15:26:15 -0700

    Extract styling to ts file

--- styles/common/Icon.tsx
+++ styles/common/Icon.tsx
@@ -1,30 +1,7 @@
-import { basename, dirname } from "path";
 import { memo, useEffect } from "react";
-import styled from "styled-components";
+import type { IconProps } from "styles/common/StyledIcon";
+import StyledIcon from "styles/common/StyledIcon";
 import { cleanUpBufferUrl } from "utils/functions";
-
-export type IconProps = {
-  displaySize?: number;
-  imgSize: number;
-};
-
-const onLoad: React.ReactEventHandler = ({ target }) =>
-  (target as HTMLImageElement).style.setProperty("visibility", "visible");
-
-const StyledIcon = styled.img.attrs<IconProps>(
-  ({ imgSize, displaySize, src = "" }) => ({
-    draggable: false,
-    height: `${displaySize || imgSize}px`,
-    onLoad,
-    src:
-      !src || src.startsWith("blob:")
-        ? src
-        : `${dirname(src)}/${imgSize}x${imgSize}/${basename(src)}`,
-    width: `${displaySize || imgSize}px`,
-  })
-)<IconProps>`
-  visibility: hidden;
-`;
 
 const Icon = (
   props: React.ImgHTMLAttributes<HTMLImageElement> & IconProps
---
+++ styles/common/StyledIcon.ts
@@ -0,0 +1,27 @@
+import { basename, dirname } from "path";
+import styled from "styled-components";
+
+export type IconProps = {
+  displaySize?: number;
+  imgSize: number;
+};
+
+const onLoad: React.ReactEventHandler = ({ target }) =>
+  (target as HTMLImageElement).style.setProperty("visibility", "visible");
+
+const StyledIcon = styled.img.attrs<IconProps>(
+  ({ imgSize, displaySize, src = "" }) => ({
+    draggable: false,
+    height: `${displaySize || imgSize}px`,
+    onLoad,
+    src:
+      !src || src.startsWith("blob:")
+        ? src
+        : `${dirname(src)}/${imgSize}x${imgSize}/${basename(src)}`,
+    width: `${displaySize || imgSize}px`,
+  })
+)<IconProps>`
+  visibility: hidden;
+`;
+
+export default StyledIcon;
